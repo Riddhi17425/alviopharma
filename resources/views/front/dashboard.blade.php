46f24c6cc1272ae59a3484181b2297b5ff05456f
Riddhi <webdeveloper9.intelliworkz@gmail.com>
2026-05-13 14:55:04 +0530

    Change

--- resources/views/front/dashboard.blade.php
+++ resources/views/front/dashboard.blade.php
@@ -159,36 +159,29 @@
         <!-- LEFT SIDE -->
         <div class="derma-section">
             <div class="derma uncapped">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'uncap']) }}"><img class=""
-                        src="public/front/images/darma-care-img_1.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'uncap']) }}"><img class="" src="{{ asset('public/front/images/darma-care-img_1.png')}}"></a>
             </div>
             <div class="derma shampoo">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'rasavio']) }}"><img class=""
-                        src="public/front/images/darma-care-img_2.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'rasavio']) }}"><img class="" src="{{ asset('public/front/images/darma-care-img_2.png')}}"></a>
             </div>
             <div class="derma supplement">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'rasaglow']) }}"><img class=""
-                        src="public/front/images/darma-care-img_3.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'rasaglow']) }}"><img class="" src="{{ asset('public/front/images/darma-care-img_3.png')}}"></a>
             </div>
         </div>
 
         <!-- RIGHT SIDE -->
         <div class="cardio-section">
             <div class="cardio cardiovascular">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'cardiovascular']) }}"><img class=""
-                        src="public/front/images/cardio-care-img_1.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'cardiovascular']) }}"><img class="" src="{{ asset('public/front/images/cardio-care-img_1.webp')}}"></a>
             </div>
             <div class="cardio diabetes">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'anti-diabetes']) }}"><img class=""
-                        src="public/front/images/cardio-care-img_2.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'anti-diabetes']) }}"><img class="" src="{{ asset('public/front/images/cardio-care-img_2.webp')}}"></a>
             </div>
             <div class="cardio urology">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'urology']) }}"><img class=""
-                        src="public/front/images/cardio-care-img_3.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'urology']) }}"><img class="" src="{{ asset('public/front/images/cardio-care-img_3.png')}}"></a>
             </div>
             <div class="cardio chronic">
-                <a href="{{ route('product', ['category' => 'all', 'division' => 'chronic-supplements']) }}"><img
-                        class="" src="public/front/images/cardio-care-img_4.png"></a>
+                <a href="{{ route('product', ['category' => 'all', 'division' => 'chronic-supplements']) }}"><img class="" src="{{ asset('public/front/images/cardio-care-img_4.png')}}"></a>
             </div>
         </div>
 
